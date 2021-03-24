a75ac25409ccb8c414e9e28e43dd4eebad62f838
kvaliant <kvaliant2001@gmail.com>
2021-03-24 13:17:37 +0800

    test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,139 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import setup_db, Actor, Movie
+
+
+class TriviaTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "capstone"
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres','admin','localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_actors(self):
+        res = self.client().get('/actors')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_get_movies(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_post_actor(self):
+        res = self.client().post('/actors',json={"name":"test","age":1,"gender":"male"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        print(data['actor']['id'])
+
+    def test_error_post_actor(self):
+        res = self.client().post('/actors',json={})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    def test_post_movie(self):
+        res = self.client().post('/movies',json={"title":"test","release_date": "2021-11-11"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_error_post_movie(self):
+        res = self.client().post('/movies',json={})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+
+    #Test works but need correct existing ID
+    '''
+    def test_patch_actor(self):
+        res = self.client().patch('/actors/1',json={"name":"test1"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_patch_movie(self):
+        res = self.client().patch('/movies/1',json={"title":"test1"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+    
+    def test_delete_actor(self):
+        res = self.client().delete('/actors/1')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_delete_movie(self):
+        res = self.client().delete('/movies/1')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+    '''
+
+    def test_error_patch_actor(self):
+        res = self.client().patch('/actors/999',json={"name":"test1"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+    def test_error_patch_movie(self):
+        res = self.client().patch('/movies/999',json={"title":"test1"})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+    def test_error_delete_actor(self):
+        res = self.client().delete('/actors/999')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+    def test_error_delete_movie(self):
+        res = self.client().patch('/movies/999')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
