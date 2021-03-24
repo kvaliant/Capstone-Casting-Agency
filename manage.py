0adffb5f501a4f8a15acbe7d54294ad51ac24f3a
kvaliant <kvaliant2001@gmail.com>
2021-03-24 21:45:37 +0800

    Heroku Issue

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Actor, Movie
+from flask_script import Manager
 
 from app import APP
-from models import db
+from models import db, Actor, Movie
 
 migrate = Migrate(APP, db)
 manager = Manager(APP)
